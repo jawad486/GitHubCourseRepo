028e5de1aa79871d64b71e4fa71351c1acb6bfb4
jawad486 <jawadqureshi486@gmail.com>
2025-01-17 18:34:31 -0500

    first commit

---
+++ src/Pages/HomePage.java
@@ -0,0 +1,7 @@
+package Pages;
+
+public class HomePage {
+	public static void main (String [] args) {
+		System.out.println("this is the home  page");
+	}
+}
---
+++ src/Pages/LoginPage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class LoginPage {
+
+	public static void main (String [] args) {
+		System.out.println("this is the login page");
+	}
+	
+}
